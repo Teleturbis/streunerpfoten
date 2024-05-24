672cf566998b539cb3d406920753ffe9c28ccc12
Kevin Poppe <kevin.poppe93@gmail.com>
2024-05-24 15:04:13 +0200

    add suspense

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -61,23 +61,25 @@ export default function BlogsOverview() {
   };
 
   return (
-    <div className='bg-white py-24 sm:py-32'>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='mx-auto max-w-2xl lg:mx-0'>
-          <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-            {blogs.title}
-          </h2>
-          <p className='mt-2 text-lg leading-8 text-gray-600'>{blogs.intro}</p>
-        </div>
-        <div className='mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
-          {!loading
-            ? posts.map((post) => <BlogCard key={post.id} post={post} />)
-            : Array.from({ length: limit }).map((_, index) => (
-                <BlogCardSkeleton key={index} />
-              ))}
-        </div>
+    <Suspense>
+      <div className='bg-white py-24 sm:py-32'>
+        <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+          <div className='mx-auto max-w-2xl lg:mx-0'>
+            <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+              {blogs.title}
+            </h2>
+            <p className='mt-2 text-lg leading-8 text-gray-600'>
+              {blogs.intro}
+            </p>
+          </div>
+          <div className='mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
+            {!loading
+              ? posts.map((post) => <BlogCard key={post.id} post={post} />)
+              : Array.from({ length: limit }).map((_, index) => (
+                  <BlogCardSkeleton key={index} />
+                ))}
+          </div>
 
-        <Suspense>
           <div className='mt-10'>
             <Pagination
               page={page}
@@ -86,8 +88,8 @@ export default function BlogsOverview() {
               changePage={(e: number) => changePage(e)}
             />
           </div>
-        </Suspense>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
